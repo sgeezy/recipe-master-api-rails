04c0dc832511b883e23bff1997518c0527af27b6
Stanford C. Goss <gmixlpixl@gmail.com>
2019-05-24 15:17:25 -0400

    add refresh controller

--- app/controllers/api/v1/signup_controller.rb
+++ app/controllers/api/v1/signup_controller.rb
@@ -7,7 +7,6 @@ module Api
           payload = { user_id: user.id }
           session = JWTSessions::Session.new(payload: payload, refresh_by_access_allowed: true)
           tokens = session.login
-
           response.set_cookie(JWTSessions.access_cookie,
                               value: tokens[:access],
                               httponly: true,
