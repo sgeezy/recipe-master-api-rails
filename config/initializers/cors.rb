bdab94fbc0c6a6f238a615e9df965ca773ed3273
Stanford C. Goss <gmixlpixl@gmail.com>
2019-06-22 21:42:38 -0400

    get cors fixed in prod

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,16 +10,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080'
-
-    resource '*',
-      headers: :any,
-      credentials: true,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
-
-  allow do
-    origins 'https://vigilant-kirch-a55168.netlify.com/'
+    origins *
 
     resource '*',
       headers: :any,
